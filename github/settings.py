e0f948ad47dedbe1ba7ccc4573dfc2837b0158d7
Anand_1209 <anandpes.ak@gmail.com>
2021-09-11 22:59:43 +0530

    Added

--- github/settings.py
+++ github/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

f3d6e5fcb3f5fea486c8846dbcf871e362539aed
Anand_1209 <anandpes.ak@gmail.com>
2021-09-12 00:29:23 +0530

    Add

--- github/settings.py
+++ github/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import django_heroku
+#import django_heroku
 from pathlib import Path
 import os
 import psycopg2
@@ -156,4 +156,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/explore'
 LOGOUT_REDIRECT_URL = '/explore'
-django_heroku.settings(locals())
+#django_heroku.settings(locals())

392d0d6b089f6d5f9a0359319df6b595baadff15
Anand_1209 <anandpes.ak@gmail.com>
2021-09-12 01:50:44 +0530

    Add

--- github/settings.py
+++ github/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/'
+STATICFILES_DIRS = (BASE_DIR,)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
